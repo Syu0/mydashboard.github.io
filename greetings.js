c1207952cf31f7f8fa4a299a23ee2f196442d5dd
juyeon <meaningful.jy@gmail.com>
2026-08-14 16:44:02 +0900

    Fix greeting message capitalization and phrasing

--- greetings.js
+++ greetings.js
@@ -21,7 +21,7 @@ function handleLoginBtnClick(event){
 }
 
 function pantinGreetings(username){
-    greeting.innerText = `Hello ${username}, Let's do we wanted to do. `;
+    greeting.innerText = `Hello ${username}, let's do what we wanted to do. `;
     greeting.classList.remove(CLASSNAME_HIDDEN);
     
 }
